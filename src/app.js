84c54ac9c09d6863c9485adf7edd02e862b754f3
yashpatel00 <yashcpatel4445@gmail.com>
2025-01-08 17:50:06 +0530

    database connection

---
+++ src/app.js
@@ -0,0 +1,37 @@
+import express  from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded({extended:true,limit:"16kb"}));
+app.use(express.static("public"))
+app.use(cookieParser)
+
+// CORS Configuration
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
+        allowedHeaders: [
+            "Content-Type",
+            "Authorization",
+            "X-Requested-With",
+            "device-remember-token",
+            "Access-Control-Allow-Origin",
+            "Origin",
+            "Accept",
+        ],
+    })
+);
+
+
+export { app };
+
+
+
+
+
+
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+    path:'./.env'
+})
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`⚙️ server running at port :${process.env.PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log("MONGO db connection failed !!", error);
+})
