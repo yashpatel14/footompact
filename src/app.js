cddbad27c18666bbe3f6a62889515ec2aa4e3956
yashpatel00 <yashcpatel4445@gmail.com>
2025-01-09 11:54:45 +0530

    user section done

--- src/app.js
+++ src/app.js
@@ -27,6 +27,16 @@ app.use(
     })
 );
 
+//import routes
+
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+
+app.use("/api/v1/users", userRouter);
+
+
+
 
 export { app };
 
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,43 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+import dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
+
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        //upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        // file has been uploaded successfull
+        // console.log("file is uploaded on cloudinary ", response.url);
+        fs.unlinkSync(localFilePath)
+        return response;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+        return null;
+    }
+}
+
+const deleteMediaFromCloudinary = async (publicId) => {
+    try {
+        await cloudinary.uploader.destroy(publicId);
+      } catch (error) {
+        console.log(error);
+      }
+}
+
+
+
+export {uploadOnCloudinary,deleteMediaFromCloudinary}
