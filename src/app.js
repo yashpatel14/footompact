81d745cb71d4c0e2744fc468229fd979bfd80efe
yashpatel00 <yashcpatel4445@gmail.com>
2025-01-09 12:33:30 +0530

    solve some error

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
-import express  from "express";
+import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(express.json({limit:"16kb"}));
-app.use(express.urlencoded({extended:true,limit:"16kb"}));
-app.use(express.static("public"))
-app.use(cookieParser)
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 // CORS Configuration
 app.use(
@@ -35,13 +35,4 @@ import userRouter from "./routes/user.routes.js";
 
 app.use("/api/v1/users", userRouter);
 
-
-
-
 export { app };
-
-
-
-
-
-
