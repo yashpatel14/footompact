244d5e9e5b46ec41cd85992868ddc4e6e6013e5c
yashpatel00 <yashcpatel4445@gmail.com>
2025-01-20 09:30:49 +0530

    solve some error

--- src/models/contact.model.js
+++ src/models/contact.model.js
@@ -21,7 +21,7 @@ const contactSchema = new Schema(
             ],
         },
         message:{
-            type:String,
+            type:[String],
             required:true
         },
         issue:{
